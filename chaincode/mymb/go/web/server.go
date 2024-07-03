4c7b594a19dd9bf4ba701b8709e6a9de208b88ae
DOBIN <devpark0810@gmail.com>
2024-07-03 12:28:25 +0900

    [ADD] update contract.go

--- chaincode/mymb/go/web/server.go
+++ chaincode/mymb/go/web/server.go
@@ -1,26 +1,42 @@
-// chaincode/mymb/go/web/server.go
 package main
 
 import (
 	"encoding/json"
 	"fmt"
-	contract "github.com/MYMB2022/mymb-bc-chaincode/chaincode/mymb/go"
 	"html/template"
 	"net/http"
 	"os/exec"
-	"path/filepath"
+	"strings"
 )
 
-// Function to execute the chaincode command
-func getAllUsers() ([]contract.User, error) {
-	// token_contract의 실제 경로로 수정합니다.
-	cmd := exec.Command(filepath.Join("..", "..", "token_contract"), "GetAllUsers")
+// User 구조체 정의 (contract 패키지에 정의된 구조체와 동일하게 맞춰야 합니다)
+type User struct {
+	UserId           string   `json:"userID"`
+	NickName         string   `json:"nickName"`
+	MymPoint         int64    `json:"mymPoint"`
+	OwnedToken       []string `json:"ownedToken"`
+	BlockCreatedTime string   `json:"blockCreatedTime"`
+}
+
+// Function to execute the Docker command and get users
+func getAllUsers() ([]User, error) {
+
+	caFilePath := "/opt/home/managedblockchain-tls-chain.pem"
+	channelID := "mychannel"
+	chaincodeName := "mycc"
+
+	// fmt.Sprintf를 사용하여 포맷된 문자열 생성
+	cmdStr := fmt.Sprintf("docker exec cli peer chaincode query --tls --cafile %s --channelID %s --name %s -c '{\"Args\":[\"GetAllUsers\"]}'", caFilePath, channelID, chaincodeName)
+	cmdArgs := strings.Fields(cmdStr)
+
+	// exec.Command를 사용하여 명령어 실행
+	cmd := exec.Command(cmdArgs[0], cmdArgs[1:]...)
 	output, err := cmd.Output()
 	if err != nil {
 		return nil, fmt.Errorf("failed to execute chaincode: %v", err)
 	}
 
-	var users []contract.User
+	var users []User
 	err = json.Unmarshal(output, &users)
 	if err != nil {
 		return nil, fmt.Errorf("failed to unmarshal response: %v", err)
